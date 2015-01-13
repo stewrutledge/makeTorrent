65ce7f964219c252db53ddd7b7e68d62a193a5f3
Stewart Rutledge <stew.rutledge@gmail.com>
2015-01-13 10:29:08 +0100

    Added setup

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+    name='makeTorrent',
+    version='0.1',
+    packages=['makeTorrent'],
+    license='Creative Commons Attribution-Noncommercial-Share Alike license',
+    description='Basic library for creating torrents',
+    long_description=open('README.rst').read(),
+    author='Stewart Rutledge',
+    author_email='stew.rutledge@gmail.com',
+    url='https://github.com/stewrutledge/makeTorrent'
+)
