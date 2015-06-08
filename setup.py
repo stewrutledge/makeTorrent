85c0b0347e711526642d6f63dc8a8e56983db06d
Stewart Rutledge <stew.rutledge@gmail.com>
2015-06-08 14:53:01 +0200

    Updated version to 0.13

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='makeTorrent',
-    version='0.11',
+    version='0.13',
     packages=['makeTorrent'],
     install_requires=[
         'bencode',
